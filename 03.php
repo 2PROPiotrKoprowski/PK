03e00531b48545096e96cd638ce43b6ac6eaab88
Piotr K <piotr.koprowski.pro@live.zs1mm.edu.pl>
2026-03-04 17:00:59 +0100

    67

--- 03.php
+++ 03.php
@@ -29,8 +29,36 @@
         echo "Dorosły mężczyzna<br>";
     }
 
-    echo($age >= 18 && plec === "kobieta") ? "Dorosła kobieta" : "Inna osoba"
+    echo($age >= 18 && plec === "kobieta") ? "Dorosła kobieta" : "Inna osoba<br>";
 
+    $dzien = 3;
+    switch($dzien){
+        case 1:
+        case 2:
+        case 3:
+        case 4:
+        case 5:
+        echo "Do szkoły<br>";
+        break;
+        case 6:
+        case 7:
+        echo "Weekend<br>";
+        break;
+        default:
+        echo "Coś źle z danymi<br>";               
+    }
+    $punkty = 88;
+    switch (true){
+        case ($punkty === 100):
+            echo "Celujący<br>";
+            break;
+        case ($punkty >= 90):
+            echo "Bardzo dobry<br>";
+            break;
+        case ($punkty >= 0):
+            echo "Ocena do poprawy<br>";
+            break;
+    }
     ?>
 </body>
 </html>
